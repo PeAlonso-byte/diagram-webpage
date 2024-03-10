7d36e35eaa5dba342091b1d28babd3937bf97d2f
Pedro Alonso <PedroAlonso@usal.es>
2024-03-10 17:32:42 +0100

    Create user.tsx
    - Create user "api"

---
+++ diagram-webpage/convex/user.tsx
@@ -0,0 +1,15 @@
+import { v } from 'convex/values'
+import { query }  from './_generated/server'
+
+export const getUser = query({
+    args: {
+        email:v.string()
+    },
+    handler:async(ctx, args) => {
+        const result = await ctx.db.query('user')
+        .filter((q) => q.eq(q.field('email'), args.email))
+        .collect() // To fetch the data
+
+        return result
+    },
+})
