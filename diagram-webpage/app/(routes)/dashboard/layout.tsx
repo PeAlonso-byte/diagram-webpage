2a3c7847d445ece11cd7906c90705ff04619ff9c
Pedro Alonso <PedroAlonso@usal.es>
2024-03-10 19:43:21 +0100

    Fixed wrong route

--- diagram-webpage/app/(routes)/dashboard/layout.tsx
+++ diagram-webpage/app/(routes)/dashboard/layout.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
+"use client"
+import { api } from '@/convex/_generated/api';
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
+import { useConvex } from 'convex/react';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react'
 
 function DashboardLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
-  }>) {
-  return (
-    <div>{children}</div>
-  )
+  }>) 
+  {
+    const { user }:any = useKindeBrowserClient()
+    const convex = useConvex()
+    const router=useRouter()
+    useEffect( () => {
+        user && checkTeam()
+    }, [user]) 
+
+    const checkTeam = async() => {
+        const result = await convex.query(api.teams.getTeam, {email: user?.email})
+    
+        if (!result?.length) {
+            router.push('teams/create')
+        }
+    }
+    return (
+        <div>{children}</div>
+    )
 }
 
 export default DashboardLayout
