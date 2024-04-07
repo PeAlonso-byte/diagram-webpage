4474887bd060f60270fbefc4645d8706a6d41a23
Pedro Alonso <PedroAlonso@usal.es>
2024-04-07 21:21:39 +0200

    Finish side nav

--- diagram-webpage/app/(routes)/dashboard/_components/SideNav.tsx
+++ diagram-webpage/app/(routes)/dashboard/_components/SideNav.tsx
@@ -1,15 +1,22 @@
-import { ChevronDown } from 'lucide-react'
+import { Archive, ChevronDown, Flag, Github } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 import SideNavTopSection from './SideNavTopSection'
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import SideNavBottomSection from './SideNavBottomSection'
 
 function SideNav() {
   const {user}:any = useKindeBrowserClient()
   return (
     <div
-    className='bg-gray-100 h-screen fixed w-72 border-r p-6'>
-        <SideNavTopSection user={user}/>
+    className='bg-gray-100 h-screen fixed w-72 border-r p-6 flex flex-col'>
+        <div className='flex-1'>
+          <SideNavTopSection user={user}/>
+        </div>
+
+        <div>
+          <SideNavBottomSection />
+        </div>
     </div>
   )
 }
--- diagram-webpage/app/(routes)/dashboard/_components/SideNavTopSection.tsx
+++ diagram-webpage/app/(routes)/dashboard/_components/SideNavTopSection.tsx
@@ -1,4 +1,4 @@
-import { ChevronDown, LogOut, Settings, User } from 'lucide-react'
+import { ChevronDown, LayoutGrid, LogOut, Settings, User } from 'lucide-react'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import {
@@ -10,6 +10,8 @@ import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
 import { Separator } from '@/components/ui/separator'
 import { useConvex } from 'convex/react'
 import { api } from '@/convex/_generated/api'
+import { useRouter } from 'next/navigation'
+import { Button } from '@/components/ui/button'
 
 export interface TEAM {
     createdBy: String,
@@ -18,6 +20,7 @@ export interface TEAM {
 }
 
 function SideNavTopSection({user}:any) {
+    const router = useRouter()
     const convex = useConvex()
     const menu = [
         {
@@ -45,54 +48,70 @@ function SideNavTopSection({user}:any) {
         setTeamList(result)
         setActiveTeam(result[0])
     }
-  return (
-    <Popover>
-        <PopoverTrigger>
-            <div className='flex items-center gap-3 hover:bg-slate-200 p-3 rounded-lg cursor-pointer'>
-                <Image src='/logo-1.png' alt='logo' width={40} height={40} />
-                <h2 className='flex gap-2 items-center font-bold text-[17px]'>{activeTeam?.teamName}
-                    <ChevronDown />
-                </h2>
-            </div>
-        </PopoverTrigger>
-        <PopoverContent className='ml-7 p-4'>
-            <div>
-                {teamList?.map((team, index) => (
-                    <h2 key={index}
-                    className='p-2 hover:bg-blue-500 hover:text-white rounded-lg mb-1 cursor-pointer'>
-                        {team.teamName}
-                    </h2>
-                ))}
-            </div>
-            <Separator className='mt-2 bg-slate-100'/>
-            <div>
-                {menu.map((item, index) => (
-                    
-                    <h2 className='flex gap-2 items-center p-2 hover:bg-gray-100 rounded-lg text-sm cursor-pointer'>
-                        <item.icon className='h-4 w-4'/>
-                        {item.name}
-                    </h2>
-                ))}
-                <LogoutLink>
 
-                    <h2 className='flex gap-2 items-center p-2 hover:bg-gray-100 rounded-lg text-sm cursor-pointer'>
-                        <LogOut className='h-4 w-4'/>
-                        Logout
+    const onMenuClick = (item:any) => {
+        if (item.path) {
+            router.push(item.path)
+        }
+    }
+  return (
+    <div>
+        <Popover>
+            <PopoverTrigger>
+                <div className='flex items-center gap-3 hover:bg-slate-200 p-3 rounded-lg cursor-pointer'>
+                    <Image src='/logo-1.png' alt='logo' width={40} height={40} />
+                    <h2 className='flex gap-2 items-center font-bold text-[17px]'>{activeTeam?.teamName}
+                        <ChevronDown />
                     </h2>
-                </LogoutLink>
-            </div>
-            <Separator className='mt-2 bg-slate-100'/>
-                { user &&
-                <div className='mt-2 flex gap-2 items-center'>
-                    <Image src={user?.picture} alt='user' width={30} height={30} className='rounded-full'/>
-                    <div>
-                        <h2 className='text-[14px] font-bold'>{user?.given_name} {user?.family_name}</h2>
-                        <h2 className='text-[12px] text-gray-500'>{user?.email}</h2>
-                    </div>
                 </div>
-                }
-        </PopoverContent>
-    </Popover>
+            </PopoverTrigger>
+            <PopoverContent className='ml-7 p-4'>
+                <div>
+                    {teamList?.map((team, index) => (
+                        <h2 key={index}
+                        className={`p-2 hover:bg-blue-500 hover:text-white rounded-lg mb-1 cursor-pointer ${activeTeam?._id==team._id&&'bg-blue-500 text-white'}`}
+                        onClick={() =>setActiveTeam(team)}>
+                            {team.teamName}
+                        </h2>
+                    ))}
+                </div>
+                <Separator className='mt-2 bg-slate-100'/>
+                <div>
+                    {menu.map((item, index) => (
+                        
+                        <h2 className='flex gap-2 items-center p-2 hover:bg-gray-100 rounded-lg text-sm cursor-pointer'
+                        onClick={() => onMenuClick(item)}>
+                            <item.icon className='h-4 w-4'/>
+                            {item.name}
+                        </h2>
+                    ))}
+                    <LogoutLink>
+
+                        <h2 className='flex gap-2 items-center p-2 hover:bg-gray-100 rounded-lg text-sm cursor-pointer'>
+                            <LogOut className='h-4 w-4'/>
+                            Logout
+                        </h2>
+                    </LogoutLink>
+                </div>
+                <Separator className='mt-2 bg-slate-100'/>
+                    { user &&
+                    <div className='mt-2 flex gap-2 items-center'>
+                        <Image src={user?.picture} alt='user' width={30} height={30} className='rounded-full'/>
+                        <div>
+                            <h2 className='text-[14px] font-bold'>{user?.given_name} {user?.family_name}</h2>
+                            <h2 className='text-[12px] text-gray-500'>{user?.email}</h2>
+                        </div>
+                    </div>
+                    }
+            </PopoverContent>
+        </Popover>
+
+        <Button variant='outline' 
+        className='w-full justify-start gap-2 font-bold mt-8 bg-gray-100'>
+            <LayoutGrid className='h-5 w-5' />
+            All Files
+        </Button>
+    </div>
   )
 }
 
