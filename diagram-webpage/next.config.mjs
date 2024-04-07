b21aa78f83a368bf1434aa72904b85aebdc92c95
Pedro Alonso <PedroAlonso@usal.es>
2024-04-07 20:59:41 +0200

    Added format to navbar

--- diagram-webpage/next.config.mjs
+++ diagram-webpage/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode:false,
+    images: {
+        remotePatterns:[{
+            protocol: 'https',
+            hostname: 'lh3.googleusercontent.com',
+            pathname: '**',
+        },]
+    }
+};
 
 export default nextConfig;
