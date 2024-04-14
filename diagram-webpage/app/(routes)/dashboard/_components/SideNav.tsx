fa7d5770dba9989d17e7f84648823e328889c026
Pedro Alonso <PedroAlonso@usal.es>
2024-04-14 19:34:17 +0200

    Fixed files count progress bar

--- diagram-webpage/app/(routes)/dashboard/_components/SideNav.tsx
+++ diagram-webpage/app/(routes)/dashboard/_components/SideNav.tsx
@@ -1,18 +1,26 @@
 import { Archive, ChevronDown, Flag, Github } from 'lucide-react'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SideNavTopSection, { TEAM } from './SideNavTopSection'
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
 import SideNavBottomSection from './SideNavBottomSection'
-import { useMutation } from 'convex/react'
+import { useConvex, useMutation } from 'convex/react'
 import { api } from '@/convex/_generated/api'
 import { toast } from 'sonner'
 
 function SideNav() {
   const {user}:any = useKindeBrowserClient()
   const createFile = useMutation(api.files.createFile)
-
   const [activeTeam, setActiveTeam] = useState<TEAM>()
+  const [totalFiles, setTotalFiles] = useState<Number>(0)
+
+  const convex = useConvex()
+
+  useEffect(() => {
+    activeTeam&&getFiles()
+  }, [activeTeam])
+
+
   const onFileCreate = (fileName:string) => {
     createFile({
       fileName:fileName,
@@ -29,6 +37,11 @@ function SideNav() {
       toast('Error while creating new File')
     })
   }
+
+  const getFiles = async () => {
+    const result = await convex.query(api.files.getFiles,{teamId:activeTeam?._id})
+    setTotalFiles(result.length)
+  }
   return (
     <div
     className='bg-gray-100 h-screen fixed w-72 border-r p-6 flex flex-col'>
@@ -38,7 +51,8 @@ function SideNav() {
 
         <div>
           <SideNavBottomSection 
-          onFileCreate={onFileCreate}/>
+          onFileCreate={onFileCreate}
+          totalFiles={totalFiles}/>
         </div>
     </div>
   )
